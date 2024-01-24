5b65828f3a321aa8d59df1d3b32a23e20abc9352
Bakeloh <bakelohnoel@gmail.com>
2024-01-24 17:19:58 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,22 @@
 
 def return_evens(num_list):
     pass
+    return [num for num in num_list if num % 2 == 0]
+
+num_list = range(10)
+result = return_evens( num_list)
+print(result)
+
 
 def make_exclamation(sentence_list):
     pass
+    return[sentence + '!' for sentence in sentence_list]
+
+sentence_list = [
+    "I like computers",
+    "I require coffee",
+    "Live long and prosper",
+]
+
+result = make_exclamation(sentence_list)
+print(result)
